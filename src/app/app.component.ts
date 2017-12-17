286073c22968af20359feefc503f11a941876ab6
ghorai77 <ghorai77@gmail.com>
2017-12-17 10:35:15 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { Subject } from 'rxjs/Subject';
+import { Observable } from 'rxjs/Rx';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  searchterm: string;
+
+  startAt = new Subject();
+  endAt = new Subject();
+
+  branchs;
+  allbranchs;
+
+  startobs = this.startAt.asObservable();
+  endobs = this.endAt.asObservable();
+
+  constructor(private afs: AngularFirestore) {
+
+  }
+
+  ngOnInit() {
+    this.getallclubs().subscribe((branchs) => {
+      this.allbranchs = branchs;
+    })
+    Observable.combineLatest(this.startobs, this.endobs).subscribe((value) => {
+      this.firequery(value[0], value[1]).subscribe((branchs
+      ) => {
+        this.branchs = branchs;
+      })
+    })
+  }
+
+  search($event) {
+    let q = $event.target.value;
+    if (q != '') {
+      this.startAt.next(q);
+      this.endAt.next(q + "\uf8ff");
+    }
+    else {
+      this.branchs = this.allbranchs;
+    }
+  }
+
+  firequery(start, end) {
+    return this.afs.collection('epl', ref => ref.limit(4).orderBy('branch').startAt(start).endAt(end)).valueChanges();
+  }
+
+  getallclubs() {
+    return this.afs.collection('epl', ref => ref.orderBy('branch')).valueChanges();
+  }
+
+
+}
+
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA205vncjVKyMNGWFLNbJtNoCStK6O0arI",
+      authDomain: "first-45ad4.firebaseapp.com",
+      databaseURL: "https://first-45ad4.firebaseio.com",
+      projectId: "first-45ad4",
+      storageBucket: "first-45ad4.appspot.com",
+      messagingSenderId: "811759470204"
+  }
+};
